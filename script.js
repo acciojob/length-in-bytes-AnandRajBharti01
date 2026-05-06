76451f2acddd36025a01aa30ab83f0268ab775b2
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-05-06 16:16:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	// let count = 0;
+	let val =  str.value;
+	let leng = val.length;
+		return leng;
+	
+	
 };
 
 // Do not change the code below
